aefc6aa17e684064fb60d69122e07ab14a75ef33
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-04-18 22:02:45 +0900

    ボタンを押すと反応するようにする

--- Breath of the Modal/Assets/Scripts/Modal/ButtonScript.cs
+++ Breath of the Modal/Assets/Scripts/Modal/ButtonScript.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour {
 
+    public Color defaultColor;
+
+    public Color pushColor;
+
+    public KeyCode code;
+
+    Material material;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        material = GetComponent<MeshRenderer>().materials[0];
+        material.color = defaultColor;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if (Input.GetKey(code))
+        {
+            material.color = pushColor;
+        }
+        else
+        {
+            material.color = defaultColor;
+        }
 	}
 }
